1699e8dceb1a1b4fca1a374d9499fff6b2db690b
Ajmal777 <81432526+Ajmal777@users.noreply.github.com>
2023-09-22 18:13:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
-//your code here
+const image = document.getElementById('user-img');
+const name = document.querySelector('.user-name');
+const getUser = document.querySelector('#getUser');
+const age = document.getElementById('age');
+const email = document.getElementById('email');
+const phone = document.getElementById('phone');
+const details = document.querySelector('.details');
+async function getData(){
+	try{
+		const data = await fetch('https://randomuser.me/api/');
+		const parsedData = await data.json();
+		return parsedData;
+	}
+	catch(e){
+		console.log(e.message);
+	}
+}
+let user;
+getUser.addEventListener('click', async () => {
+	user = await getData();
+	image.setAttribute('src', user.results[0].picture.large);
+	name.innerText = user.results[0].name.first + " " + user.results[0].name.last;
+})
+
+age.addEventListener('click', () =>{
+	details.innerText = user.results[0].dob.age;
+})
+email.addEventListener('click', () =>{
+	details.innerText = user.results[0].email;
+})
+phone.addEventListener('click', () =>{
+	details.innerText = user.results[0].phone;
+})
+
+
+
